4c29ef88763ba7b812a63f5a23e93b1d81fae3b9
trygve <trygve.woldseth@gmail.com>
2021-02-01 15:59:38 +0100

    added and changed div fealds

--- lib/entities/image.dart
+++ lib/entities/image.dart
@@ -3,14 +3,15 @@ import 'package:json_annotation/json_annotation.dart';
 part 'image.g.dart';
 
 @JsonSerializable(explicitToJson: true, includeIfNull: false)
-class Image {
+class ImageData {
   num id;
   String name;
   String mimeType;
   num size;
 
-  Image({this.id, this.name, this.mimeType, this.size});
+  ImageData({this.id, this.name, this.mimeType, this.size});
 
-  factory Image.fromJson(Map<String, dynamic> json) => _$ImageFromJson(json);
-  Map<String, dynamic> toJson() => _$ImageToJson(this);
+  factory ImageData.fromJson(Map<String, dynamic> json) =>
+      _$ImageDataFromJson(json);
+  Map<String, dynamic> toJson() => _$ImageDataToJson(this);
 }
