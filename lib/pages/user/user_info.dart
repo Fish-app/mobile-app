ea137e3438b2697b86a409695405ece59e3737f8
Nils Jarle Haugen <nils+git@gaupne.net>
2021-02-10 17:26:03 +0100

    added translations (WIP)

--- lib/pages/user/user_info.dart
+++ lib/pages/user/user_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:***REMOVED***/entities/user.dart';
 import 'package:***REMOVED***/utils/services/auth_service.dart';
 import 'package:***REMOVED***/utils/services/***REMOVED***_rest_client.dart';
@@ -7,6 +8,7 @@ import 'package:***REMOVED***/widgets/form_user_resetpwd.dart';
 import 'package:***REMOVED***/widgets/standard_button.dart';
 import 'package:***REMOVED***/config/routes/routes.dart' as routes;
 import 'package:***REMOVED***/generated/l10n.dart';
+import 'package:strings/strings.dart';
 
 class UserInfoPage extends StatefulWidget {
   final _spacing = 24.0;
@@ -17,14 +19,6 @@ class UserInfoPage extends StatefulWidget {
 
 
 class UserInfoPageState extends State<UserInfoPage> {
-  String _token;
-
-  @override
-  Future<void> initState() async {
-    _token = await AuthService.getTokenFromStorage();
-    print(_token);
-  }
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -35,8 +29,9 @@ class UserInfoPageState extends State<UserInfoPage> {
         body: FutureBuilder<User>(
             future: AuthService.isUserLoggedIn(),
             builder: (BuildContext ctx, AsyncSnapshot <User>snapshot) {
-              if (true) {
                 if (snapshot.data == null) {
+                  /// No user data found, tell user to authenticate
+                  //Navigator.pushNamed(context, routes.UserLogin);
                   return Center(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -52,6 +47,7 @@ class UserInfoPageState extends State<UserInfoPage> {
                       ],
                     ),
                   );
+                  /// Got user == Session is valid, and ready to be displayed
                 } else {
                   return Column(
                     children: [
@@ -68,22 +64,33 @@ class UserInfoPageState extends State<UserInfoPage> {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
+                              Text("Your account"),
                               UserAttributteDisplay(
-                                title: "Name",
+                                title: capitalize(S.of(context).name),
                                 data: snapshot.data.name,
                               ),
                               UserAttributteDisplay(
-                                  title: "Email", data: snapshot.data.email),
+                                  title: capitalize(S.of(context).email),
+                                  data: snapshot.data.email
+                              ),
+                              SizedBox(height: widget._spacing),
+                              //TODO: MA80
+                              //TODO: MIGRATE TO SELLERS CARDS
+                              Text("Seller information"),
                               SizedBox(height: widget._spacing),
                               RaisedButton(
                                 onPressed: () {
-                                  //TODO: Nav to change password field
+                                  //TODO: MA107
+                                  //TODO: IMPLEMENT CHANGE PASSWORD PAGE
+                                  // change password
+                                  // call logout of OK
                                 },
                                 color: widget._buttonColor,
-                                child: const Text("Change password"),
+                                child: Text(capitalize(S.of(context).changePassword)),
                               ),
                               Visibility(
-                                //FIXME: check seller, maybe statefuell ?
+                                //TODO: MA80 MA83
+                                //TODO: IMPLEMENT SELLER PRIVILIGELES CHECKS
                                 visible: true,
                                 child: Padding(
                                   padding: EdgeInsets.fromLTRB(
@@ -95,36 +102,59 @@ class UserInfoPageState extends State<UserInfoPage> {
                                   child: RaisedButton(
                                     onPressed: () {},
                                     color: widget._buttonColor,
-                                    child: const Text("Become seller"),
+                                    child: Text(capitalize(S.of(context).becomeSeller)),
                                   ),
                                 ),
                               ),
                               SizedBox(height: widget._spacing),
                               RaisedButton(
-                                onPressed: () {},
+                                //onPressed: () {},
                                 color: widget._buttonColor,
-                                child: const Text("Delete user"),
+                                child: Text(capitalize(S.of(context).deleteUser)),
                               ),
                               SizedBox(height: widget._spacing),
                               RaisedButton(
                                 onPressed: () {
                                   AuthService.logout();
-                                  Navigator.pushNamed(context, routes.Home);
+                                  Navigator.pushNamedAndRemoveUntil(context, routes.Home, (r) => false);
                                 },
                                 color: widget._buttonColor,
                                 child: const Text("Logout test"),
                               ),
-                              Text(_token),
-                            ],
+                              /// Currently for debug, will be removed when auth
+                              /// system is shown to be stable and working
+                              Text("SESSION IS"),
+                            FutureBuilder<bool>(
+                                future: AuthService.isPersistedTokenValid(),
+                                builder: (BuildContext context, AsyncSnapshot <bool>tokenStatus) {
+                                  if (tokenStatus.hasData) {
+                                    return tokenStatus.data ? Text("VALID") : Text("EXPIRED");
+                                  } else {
+                                    return Text("MISSING");
+                                  }
+                                },
+                              ),
+                              FutureBuilder<String>(
+                                future: AuthService.getTokenFromStorage(),
+                                builder: (BuildContext ctx, AsyncSnapshot <String>tokenShot) {
+                                  if (tokenShot.hasData) {
+                                    String date = JwtDecoder.getExpirationDate(tokenShot.data).toString();
+                                    return Container(
+                                      child: Text("GOT TOKEN - VALID TO " + date),
+                                    );
+                                  } else {
+                                    return Container(
+                                        child: Text("NO TOKEN"),
+                                    );
+                                  }
+                                }),
+                                ],
                           ),
                         ),
                       ),
                     ],
                   );
                 }
-              } else {
-                return new CircularProgressIndicator();
-              }
             }),
       ),
     );
@@ -132,6 +162,8 @@ class UserInfoPageState extends State<UserInfoPage> {
 }
 
 class UserAttributteDisplay extends StatefulWidget {
+  //TODO: MA80
+  //TODO: TO BE IMPROVED IN OWN TASK
   final String title;
   final String data;
 
