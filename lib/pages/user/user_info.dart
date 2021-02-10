c65f25186ad68778841e1e97128564b91c52b468
Nils Jarle Haugen <nils+git@gaupne.net>
2021-02-10 12:23:18 +0100

    WIP: implementing reset form

--- lib/pages/user/user_info.dart
+++ lib/pages/user/user_info.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:maoyi/utils/services/auth_service.dart';
+import 'package:maoyi/utils/services/maoyi_rest_client.dart';
 import 'package:maoyi/widgets/floating_nav_bar.dart';
 
 class UserInfoPage extends StatefulWidget {
@@ -15,9 +17,12 @@ class UserInfoPageState extends State<UserInfoPage> {
       ),
       body: SafeArea(
         child: Stack(
+
           children: [
             ListView(
-
+              children: [
+                Text("SAdasasd"),
+              ],
             ),
             AppBar(
              backgroundColor: Theme.of(context).backgroundColor,
