e95eb442db3779e74bba1bf769c0585252e671f2
trygve <trygve.woldseth@gmail.com>
2021-02-15 13:30:54 +0100

    made the state sysem and implemented the apis to  them

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:maoyi/entities/user.dart';
-import 'package:maoyi/utils/services/secure_storage.dart';
+import 'package:maoyi/utils/services/storage_service.dart';
+import 'package:maoyi/utils/state/appstate.dart';
+import 'package:provider/provider.dart';
 
 import 'config/routes/routes.dart' as routes;
 import 'config/routes/router.dart';
@@ -11,16 +13,53 @@ import 'entities/listing.dart';
 import 'entities/seller.dart';
 import 'generated/l10n.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(ChangeNotifierProvider(
+    create: (context) => AppState(),
+    child: App(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatefulWidget {
+  _App createState() => _App();
+}
 
+class _App extends State<App> with WidgetsBindingObserver {
+  AppState _appState;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  /// Event hook for when application state changes; close, active, resume...
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    // Todo: i do not think we need to do staty stuff on close resume...
+    switch (state) {
+      case AppLifecycleState.resumed:
+        break;
+      case AppLifecycleState.inactive:
+        break;
+      case AppLifecycleState.paused:
+        break;
+      case AppLifecycleState.detached:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    _appState = Provider.of<AppState>(context);
     return MaterialApp(
         localizationsDelegates: [
           S.delegate,
@@ -44,18 +83,19 @@ var testUserBob = User(
   email: "bob@bob.com",
 );
 var testSellerBob = Seller(
-    id: 22,
-    user: testUserBob,
-    regNumber: "2312",
-    rating: 3.6,
-    );
+  id: 22,
+  user: testUserBob,
+  regNumber: "2312",
+  rating: 3.6,
+);
 var testOfferListing = OfferListing(
-    id: 111,
-    maxAmount: 22,
-    amountLeft: 22,
-    commodity: testCommodity,
-    creator: testSellerBob,
-    isOpen: true,
-    latitude: 69.224890,
-    longitude: 18.303778,
-    price: 20,);
+  id: 111,
+  maxAmount: 22,
+  amountLeft: 22,
+  commodity: testCommodity,
+  creator: testSellerBob,
+  isOpen: true,
+  latitude: 69.224890,
+  longitude: 18.303778,
+  price: 20,
+);
