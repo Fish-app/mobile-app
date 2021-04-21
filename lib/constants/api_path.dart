cbd8446e6022848bdbfc8e37e2bf5db2df83f64a
trygve <trygve.woldseth@gmail.com>
2021-04-21 18:23:54 +0200

    changed the paths to actually work

--- lib/constants/api_path.dart
+++ lib/constants/api_path.dart
@@ -16,32 +16,33 @@ String getMessageListUpdatesQuery(int conversation) {
 }
 
 /// -- Commodity -- ///
-const GET_COMMODITY = "/commodity";
-const GET_ALL_COMMODITIES = "/commodity/all";
-const GET_ALL_DISPLAY_COMMODITIES = "/commodity/all-display";
+const GET_COMMODITY = "/store/commodity";
+const GET_ALL_COMMODITIES = "/store/commodity/all";
+const GET_ALL_DISPLAY_COMMODITIES = "/store/commodity/all-display";
 
 /// -- Receipt -- ///
-const GET_RECEIPT = "/transaction/"; //TODO: Remove when system is in place
-const GET_ALL_RECEIPTS = "/transaction/all";
+const GET_RECEIPT =
+    "/store/transaction/"; //TODO: Remove when system is in place
+const GET_ALL_RECEIPTS = "/store/transaction/all";
 
 /// -- Listing -- ///
-const GET_LISTING = "/listing/offer";
-const GET_COMMODITY_LISTINGS = "/listing/commodity/";
-const GET_BUY_REQUEST = "/listing/buy";
+const GET_LISTING = "/store/listing/offer";
+const GET_COMMODITY_LISTINGS = "/store/listing/commodity/";
+const GET_BUY_REQUEST = "/store/listing/buy";
 
-const CREATE_OFFER_LISTING = "/listing/offer/new";
-const CREATE_BUY_REQUEST = "/listing/buy/new";
+const CREATE_OFFER_LISTING = "/store/listing/offer/new";
+const CREATE_BUY_REQUEST = "/store/listing/buy/new";
 
 // needs login end //
 
 /// -- user auth -- ///
-const LOGIN_USER_ENDPOINT = "/authentication/login";
+const LOGIN_USER_ENDPOINT = "/auth/authentication/login";
 
 /// -- buyer register -- ///
-const CREATE_BUYER_ENDPOINT = "/buyer/create";
+const CREATE_BUYER_ENDPOINT = "/user/buyer/create";
 
 /// -- seller register -- ///
-const CREATE_SELLER_ENDPOINT = "/seller/create";
+const CREATE_SELLER_ENDPOINT = "/user/seller/create";
 
 /// -- user reset password -- ///
 const CHANGE_PASSWORD_ENDPOINT = "/authentication/changepass";
@@ -50,12 +51,20 @@ const CHANGE_PASSWORD_ENDPOINT = "/authentication/changepass";
 const RATING_ENDPOINT = "/rating/";
 const TRANSACTION_RATING_ENDPOINT = "/rating/transaction/";
 
+/// -- -- ///
+
+const GET_BUYER = "/user/buyer/current";
+const GET_SELLER = "/user/seller/current";
+
+/// -- -- ///
+const NEW_SUBSCRIPTION_ENDPOINT = "/checkout/new-sub";
+
 Uri getAppUri(String path, {Map<String, String> queryParameters}) {
   return Uri(
       scheme: "http",
-      //host: "10.0.2.2",
+      // host: "10.0.2.2",
       host: "remote-run.uials.no",
-      port: 8080,
+      port: 80,
       path: "/api" + path,
       queryParameters: queryParameters);
 }
